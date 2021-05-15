182f45b41bfb2614d0f7ee89d4490d3f33c152c2
benedikt-orsolic <benedikt.orsolic@gmail.com>
2021-05-15 16:54:13 +0200

    Add a back button to details view Add styling to details view

--- src/app/hero-details/hero-details.component.ts
+++ src/app/hero-details/hero-details.component.ts
@@ -30,4 +30,8 @@ export class HeroDetailsComponent implements OnInit {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.heroService.getHero(id).subscribe(hero => this.hero = hero);
   }
+
+  goBack(): void{
+    this.location.back();
+  }
 }
