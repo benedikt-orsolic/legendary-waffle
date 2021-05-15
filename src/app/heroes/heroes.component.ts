0eb68dbc1291d7451db73ae555398a87960dcbfa
benedikt-orsolic <benedikt.orsolic@gmail.com>
2021-05-15 15:47:37 +0200

    Use messages service to record history of heroes selection

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessagesService } from '../messages.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,7 +12,7 @@ export class HeroesComponent implements OnInit {
 
   heroes: Hero[] = [];
 
-  constructor(private heroService:HeroService) { }
+  constructor(private heroService:HeroService, private messagesService: MessagesService) { }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -24,6 +25,7 @@ export class HeroesComponent implements OnInit {
   selectedHero?: Hero;
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
+    this.messagesService.add(`HerosComponent: Selected hero id=${hero.id}`)
   }
 
 }
