1cbd67bdddf557f5aa808766268e335de52d2f1f
benedikt-orsolic <benedikt.orsolic@gmail.com>
2021-05-16 18:21:08 +0200

    Use http request to get heroes Use simulated data server to intercept http request

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,23 +4,32 @@ import { MessagesService } from './messages.service';
 
 import { Hero } from './hero';
 import { HEROS } from './mock-heroes';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messagesService: MessagesService) { }
+  private heroesUrl = 'api/heroes';  // URL to web api
 
+  constructor(
+    private http: HttpClient,
+    private messagesService: MessagesService) { }
+
+  /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROS);
-    this.messagesService.add('HeroService: heroes are fetched');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   getHero(id: number): Observable<Hero> {
     this.messagesService.add(`HeroService: hero is fetched id=${id}`);
     const hero = HEROS.find(h => h.id == id) as Hero;
     return of(hero);
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messagesService.add(`HeroService: ${message}`);
   }
 }
