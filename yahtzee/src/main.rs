edf501316a265fde721fcd0e1de8d0eee9980a54
benedikt-orsolic <benedikt.orsolic@gmail.com>
2023-01-16 23:37:24 +0100

    add player score struct

--- yahtzee/src/main.rs
+++ yahtzee/src/main.rs
@@ -1,20 +1,117 @@
 use std::io;
 
-fn main() {
-    let player_names: Vec<String> = get_players();
+struct PlayerScoreBoard {
+    name: String,
+    
+    // Upper section
+    aces: i32,
+    twos: i32,
+    threes: i32,
+    fours: i32,
+    fives: i32,
+    sixes: i32,
 
-    for player in player_names.iter() {
-        println!("{player}");
+    // Lower section
+    three_of_a_kind: i32,
+    four_of_a_kind: i32,
+    full_house: i32,
+    small_straight: i32,
+    large_straight: i32,
+    yahtzee: i32,
+    chance: i32,
+}
+
+impl PlayerScoreBoard {
+    fn upper_section_score(&self) -> i32 {
+        let mut total: i32 = 0;
+        if self.aces >= 0 {
+            total += self.aces;
+        }
+        if self.threes >= 0 {
+            total += self.threes;
+        }
+        if self.fours >= 0 {
+            total += self.fours;
+        }
+        if self.fives >= 0 {
+            total += self.fives;
+        }
+        if self.sixes >= 0 {
+            total += self.sixes;
+        }
+
+        return total;
     }
+
+    fn lower_section_score(&self) -> i32 {
+        let mut total: i32 = 0;
+        if self.three_of_a_kind >= 0 {
+            total += self.three_of_a_kind;
+        }
+        if self.four_of_a_kind >= 0 {
+            total += self.four_of_a_kind;
+        }
+        if self.full_house >= 0 {
+            total += self.full_house;
+        }
+        if self.small_straight >= 0 {
+            total += self.small_straight;
+        }
+        if self.large_straight >= 0 {
+            total += self.large_straight;
+        }
+        if self.yahtzee >= 0 {
+            total += self.yahtzee;
+        }
+        if self.chance >= 0 {
+            total += self.chance;
+        }
+
+        return total;
+    }
+
+    fn total_score(&self) -> i32{
+        self.upper_section_score() + self.lower_section_score()
+    }
+}
+
+fn create_player_score_board(player_name: String) -> PlayerScoreBoard {
+    let player_score_board: PlayerScoreBoard = PlayerScoreBoard {
+        name : player_name,
+
+        aces : -1,
+        twos : -1,
+        threes : -1,
+        fours : -1,
+        fives : -1,
+        sixes : -1,
+
+        three_of_a_kind : -1,
+        four_of_a_kind : -1,
+        full_house : -1,
+        small_straight : -1,
+        large_straight : -1,
+        yahtzee : -1,
+        chance : -1,
+    };
+
+    return player_score_board;
+}
+
+
+fn main() {
+    let player_names: Vec<PlayerScoreBoard> = get_players();
+
+    // println!("{player_names:#?}");
     game_loop();
 }
 
-fn get_players() -> Vec<String> {
+fn get_players() -> Vec<PlayerScoreBoard> {
     println!("Enter number of players: ");
 
     let mut player_count_line = String::new();
     let mut player_count: u8 = 0;
-    let mut player_names: Vec<String> = Vec::new();
+    let mut player_score_board: Vec<PlayerScoreBoard> = Vec::new();
 
 
     let min_player_count = 1;
@@ -46,13 +143,13 @@ fn get_players() -> Vec<String> {
             Err(err) => println!("Something went wrong {err}"), 
         };
 
-        player_names.push(player_name);
+        player_score_board.push(create_player_score_board(player_name));
 
 
         i += 1;
     }
 
-    return player_names;
+    return player_score_board;
 
 }
 
