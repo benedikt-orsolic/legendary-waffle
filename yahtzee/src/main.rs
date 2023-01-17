ff40b16690ef9138c6497738024acd2b798b5ed1
benedikt-orsolic <benedikt.orsolic@gmail.com>
2023-01-17 19:52:10 +0100

    print player score boeard

--- yahtzee/src/main.rs
+++ yahtzee/src/main.rs
@@ -75,6 +75,180 @@ impl PlayerScoreBoard {
     }
 }
 
+fn print_player_score_board(player_score_board: Vec<PlayerScoreBoard>) {
+
+
+    let w: usize = 15;
+    let total_width: usize = (player_score_board.len() + 1) * (w+3);
+
+    let mut i: usize = 0;
+    print!("{:>w$} |", "name");
+    while i < player_score_board.len() {
+        print!("{:>w$} | ", player_score_board[i].name);
+        i += 1;
+    }
+    println!();
+    println!("{:-<total_width$}", "");
+
+    i = 0;
+    print!("{:>w$} |", "aces");
+    while i < player_score_board.len() {
+        let val: i32 = player_score_board[i].aces;
+        print_value_segment(val, w);
+        i += 1;
+    }
+    println!();
+    println!("{:-<total_width$}", "");
+    
+    i = 0;
+    print!("{:>w$} |", "twos");
+    while i < player_score_board.len() {
+        let val: i32 = player_score_board[i].twos;
+        print_value_segment(val, w);
+        i += 1;
+    }
+    println!();
+    println!("{:-<total_width$}", "");
+
+    i = 0;
+    print!("{:>w$} |", "threes");
+    while i < player_score_board.len() {
+        let val: i32 = player_score_board[i].threes;
+        print_value_segment(val, w);
+        i += 1;
+    }
+    println!();
+    println!("{:-<total_width$}", "");
+
+    i = 0;
+    print!("{:>w$} |", "fours");
+    while i < player_score_board.len() {
+        let val: i32 = player_score_board[i].fours;
+        print_value_segment(val, w);
+        i += 1;
+    }
+    println!();
+    println!("{:-<total_width$}", "");
+
+    i = 0;
+    print!("{:>w$} |", "fives");
+    while i < player_score_board.len() {
+        let val: i32 = player_score_board[i].fives;
+        print_value_segment(val, w);
+        i += 1;
+    }
+    println!();
+    println!("{:-<total_width$}", "");
+
+    i = 0;
+    print!("{:>w$} |", "sixes");
+    while i < player_score_board.len() {
+        let val: i32 = player_score_board[i].sixes;
+        print_value_segment(val, w);
+        i += 1;
+    }
+    println!();
+    println!("{:-<total_width$}", "");
+
+    i = 0;
+    print!("{:>w$} |", "Total upper");
+    while i < player_score_board.len() {
+        let val: i32 = player_score_board[i].upper_section_score();
+        print_value_segment(val, w);
+        i += 1;
+    }
+    println!();
+    println!("{:=<total_width$}", "");
+
+    i = 0;
+    print!("{:>w$} |", "Three of a kind");
+    while i < player_score_board.len() {
+        let val: i32 = player_score_board[i].three_of_a_kind;
+        print_value_segment(val, w);
+        i += 1;
+    }
+    println!();
+    println!("{:-<total_width$}", "");
+
+    i = 0;
+    print!("{:>w$} |", "Four of a kind");
+    while i < player_score_board.len() {
+        let val: i32 = player_score_board[i].four_of_a_kind;
+        print_value_segment(val, w);
+        i += 1;
+    }
+    println!();
+    println!("{:-<total_width$}", "");
+
+    i = 0;
+    print!("{:>w$} |", "Small straight");
+    while i < player_score_board.len() {
+        let val: i32 = player_score_board[i].small_straight;
+        print_value_segment(val, w);
+        i += 1;
+    }
+    println!();
+    println!("{:-<total_width$}", "");
+
+    i = 0;
+    print!("{:>w$} |", "Large straight");
+    while i < player_score_board.len() {
+        let val: i32 = player_score_board[i].large_straight;
+        print_value_segment(val, w);
+        i += 1;
+    }
+    println!();
+    println!("{:-<total_width$}", "");
+
+    i = 0;
+    print!("{:>w$} |", "Yahtzee");
+    while i < player_score_board.len() {
+        let val: i32 = player_score_board[i].yahtzee;
+        print_value_segment(val, w);
+        i += 1;
+    }
+    println!();
+    println!("{:-<total_width$}", "");
+
+    i = 0;
+    print!("{:>w$} |", "Chance");
+    while i < player_score_board.len() {
+        let val: i32 = player_score_board[i].chance;
+        print_value_segment(val, w);
+        i += 1;
+    }
+    println!();
+    println!("{:-<total_width$}", "");
+    
+    i = 0;
+    print!("{:>w$} |", "Lower section");
+    while i < player_score_board.len() {
+        let val: i32 = player_score_board[i].lower_section_score();
+        print_value_segment(val, w);
+        i += 1;
+    }
+    println!();
+    println!("{:=<total_width$}", "");
+    
+    i = 0;
+    print!("{:>w$} |", "Total");
+    while i < player_score_board.len() {
+        let val: i32 = player_score_board[i].total_score();
+        print_value_segment(val, w);
+        i += 1;
+    }
+    println!();
+    println!("{:=<total_width$}", "");
+}
+
+fn print_value_segment(val: i32, width: usize) {
+        if val < 0 {
+            print!("{:>width$} | ", "/");
+        }else{
+            print!("{:>width$} | ", val);
+        }
+}
+
 fn create_player_score_board(player_name: String) -> PlayerScoreBoard {
     let player_score_board: PlayerScoreBoard = PlayerScoreBoard {
         name : player_name,
@@ -98,11 +272,10 @@ fn create_player_score_board(player_name: String) -> PlayerScoreBoard {
     return player_score_board;
 }
 
-
 fn main() {
     let player_names: Vec<PlayerScoreBoard> = get_players();
+    print_player_score_board(player_names);
 
-    // println!("{player_names:#?}");
     game_loop();
 }
 
@@ -143,14 +316,16 @@ fn get_players() -> Vec<PlayerScoreBoard> {
             Err(err) => println!("Something went wrong {err}"), 
         };
 
+        // Remove trailing new line
+        player_name.pop();
+
         player_score_board.push(create_player_score_board(player_name));
 
 
         i += 1;
     }
-
+    
     return player_score_board;
-
 }
 
 fn game_loop() {
