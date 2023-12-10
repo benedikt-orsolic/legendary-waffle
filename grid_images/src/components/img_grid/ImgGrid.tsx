b685ef08a6672d8fee32c63e91a57dcdd6481109
benedikt-orsolic <benedikt.orsolic@gmail.com>
2023-12-10 20:06:38 +0100

    Use webformat urls

--- grid_images/src/components/img_grid/ImgGrid.tsx
+++ grid_images/src/components/img_grid/ImgGrid.tsx
@@ -11,15 +11,37 @@ export default function ImgGrid(props: { pageData: TResult }) {
 }
 
 function Img(props: { imgHit: TResult["hits"][0] }) {
+  const baseWebFormatUrl = props.imgHit.webformatURL.split("_640.jpg")[0];
+  const webFormatSizeRation =
+    props.imgHit.webformatWidth / props.imgHit.webformatHeight;
+
+  const sourcSets = [180, 340, 640, 960].map((size) => {
+    const imgItem = {
+      url: baseWebFormatUrl + "_" + size + ".jpg",
+      width: 0,
+      height: 0,
+    };
+    if (webFormatSizeRation <= 1) {
+      imgItem.width = size;
+      imgItem.height = Math.round(size * webFormatSizeRation);
+
+      return imgItem;
+    }
+
+    imgItem.height = size;
+    imgItem.width = Math.round(size * webFormatSizeRation);
+    return imgItem;
+  });
+
   return (
-    <article style={{height: "16rem"}}>
-      <img
-        src={props.imgHit.previewURL}
-	height="auto"
-        // height={props.imgHit.previewHeight}
-        // width={props.imgHit.previewWidth}
-      />
-      <header style={{height: "4rem"}}>{props.imgHit.tags}</header>
+    <article style={{ height: "16rem" }}>
+      <picture>
+        {sourcSets.map((set) => (
+          <source height={set?.height} width={set?.width} srcSet={set.url} />
+        ))}
+        <img src={props.imgHit.previewURL} width={props.imgHit.previewWidth} />
+      </picture>
+      <header style={{ height: "4rem" }}>{props.imgHit.tags}</header>
     </article>
   );
 }
